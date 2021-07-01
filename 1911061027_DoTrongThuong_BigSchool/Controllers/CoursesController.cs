f2172a978ea1000f7893946fa53e1fa21f80bf31
trongthuong96 <dotrongthuong8@gmail.com>
2021-07-01 10:06:42 +0700

    thêm _dbContext sửa hàm Create

--- 1911061027_DoTrongThuong_BigSchool/Controllers/CoursesController.cs
+++ 1911061027_DoTrongThuong_BigSchool/Controllers/CoursesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _1911061027_DoTrongThuong_BigSchool.Models;
+using _1911061027_DoTrongThuong_BigSchool.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,21 @@ namespace _1911061027_DoTrongThuong_BigSchool.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
         // GET: Courses
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
         }
     }
 }
