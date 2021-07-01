67f8a88c2069ee83572cefd777edefc1db2dca00
trongthuong96 <dotrongthuong8@gmail.com>
2021-07-01 09:52:55 +0700

    Tạo get set của Place, Date, Time

---
+++ 1911061027_DoTrongThuong_BigSchool/ViewModels/CourseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1911061027_DoTrongThuong_BigSchool.ViewModels
+{
+    public class CourseViewModel
+    {
+        public string Place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+    }
+}
