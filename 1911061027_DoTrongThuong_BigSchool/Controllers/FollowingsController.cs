ce8ddcec5cfefdcb1295dfe28d0a73c4312163aa
trongthuong96 <dotrongthuong8@gmail.com>
2021-07-02 15:45:09 +0700

    Theo dõi Giảng viên : Follow

---
+++ 1911061027_DoTrongThuong_BigSchool/Controllers/FollowingsController.cs
@@ -0,0 +1,39 @@
+﻿using _1911061027_DoTrongThuong_BigSchool.DTOs;
+using _1911061027_DoTrongThuong_BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+
+
+namespace _1911061027_DoTrongThuong_BigSchool.Controllers
+{
+    public class FollowingsController : ApiController
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public FollowingsController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto followingDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Followings.Any(f => f.FollowerId == userId && f.FolloweeId == followingDto.FolloweeId))
+                return BadRequest("Following already exists!");
+
+            var folowing = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = followingDto.FolloweeId
+            };
+
+            _dbContext.Followings.Add(folowing);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ 1911061027_DoTrongThuong_BigSchool/DTOs/FollowingDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1911061027_DoTrongThuong_BigSchool.DTOs
+{
+    public class FollowingDto
+    {
+        public string FolloweeId { get; set; }
+    }
+}
